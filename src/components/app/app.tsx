6318eea635020af76fd271feb03c83a7627faa61
achiewe <mamukashviliachi706@gmail.com>
2024-04-10 16:27:40 +0400

    create new folder pages where is two main page

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,19 @@
-import React from "react";
 import Header from "../header/header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const App: React.FC = () => {
+function App() {
   return (
     <>
       <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/garage" element={<Garage />} />
+          <Route path="/winners" element={<Winners />} />
+        </Routes>
+      </Router>
     </>
   );
-};
+}
 
 export default App;
