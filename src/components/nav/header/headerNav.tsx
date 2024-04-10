dc28bee01ea2643d0a37bbde5ff8e159c4fa0c8d
achiewe <mamukashviliachi706@gmail.com>
2024-04-10 22:03:49 +0400

    add elements in the headerNav component and style it

--- src/components/nav/header/headerNav.tsx
+++ src/components/nav/header/headerNav.tsx
@@ -1,5 +1,12 @@
-function HeaderNav() {
-  return <div> </div>;
+import PropsHeader from "../../../types/propsHeader";
+import "./headerNav.css";
+
+function HeaderNav({ pages, pageStatus }: PropsHeader) {
+  return (
+    <nav className="nav">
+      <ul className="nav-list"></ul>
+    </nav>
+  );
 }
 
 export default HeaderNav;
