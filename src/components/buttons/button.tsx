5677e719a726d75c8657276bd030d467da6965de
achiewe <mamukashviliachi706@gmail.com>
2024-04-10 17:03:40 +0400

    create button css and tsx and style it and also in the types folder add propsButton type

---
+++ src/components/buttons/button.tsx
@@ -0,0 +1,18 @@
+import PropsButton from "../../types/propsButton";
+
+import "./button.css";
+
+function Button({ className, text, disabled, handleClick }: PropsButton) {
+  return (
+    <button
+      className={`${className} ${disabled ? "btn-disabled" : ""}`}
+      type="button"
+      disabled={disabled}
+      onClick={handleClick}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default Button;
